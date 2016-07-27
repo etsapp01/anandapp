439e5e8174978f6229cd8728ec5f0df965a2d31c
Hiren <hirenpatel.1088@gmail.com>
2016-07-28 02:22:39 +0530

    changes

---
+++ app/src/main/java/gujaratcm/anandiben/activerecordbase/ARConst.java
@@ -0,0 +1,11 @@
+package gujaratcm.anandiben.activerecordbase;
+
+/**
+ * Active Record constants
+ * 
+ * @author Vladimir Kroz
+ * 
+ */
+public class ARConst {
+	public static String TAG = "AR";
+}
